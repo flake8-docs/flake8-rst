2c3f46dc85a8d85f956880fa5d09d07406b1b0c4
Denis Kataev <denis.a.kataev+git@gmail.com>
2018-10-29 12:54:43 +0500

    Bump version: 0.4.2 → 0.4.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='flake8-rst',
-    version='0.4.2',
+    version='version='0.4.2'',
     license="MIT",
     packages=find_packages(),
     url='https://github.com/kataev/flake8-rst',
