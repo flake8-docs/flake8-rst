78f77a218b5dbc8691eb2b408d051e46c90f7474
Fabian Haase <haase.fabian@gmail.com>
2018-11-15 13:42:58 +0100

    adding test
    Check whether RstFileChecker swaps StyleGuide's
    DecisionEngine before returning results.
    
    Signed-off-by: Fabian Haase <haase.fabian@gmail.com>

--- tests/test_inject_options.py
+++ tests/test_inject_options.py
@@ -1,9 +1,11 @@
+import sys
+
 import optparse
 import pytest
 from hypothesis import given
 from hypothesis import strategies as st
 
-from flake8_rst.checker import inject_options
+from flake8_rst.checker import inject_options, RstFileChecker
 
 
 @given(key=st.sampled_from(['ignore', 'select']))
@@ -28,3 +30,19 @@ def test_add_options(key, values, expected):
 
     assert expected.sort() == injected.__dict__[key].sort()
     assert ['F821', 'E305'] == options.__dict__[key]
+
+
+@pytest.mark.skipif(sys.version_info < (3, 3), reason="requires python3.3 or higher")
+def test_selecting_decision_engine():
+    from unittest.mock import Mock
+    style_guide = Mock(decider=Mock())
+    decider = Mock()
+    options = Mock(max_line_length=80, verbose=0, hang_closing=False)
+
+    checker = RstFileChecker('dummy.py', {}, options)
+    checker.style_guide = style_guide
+    checker.decider = decider
+
+    checker.results
+
+    assert decider is style_guide.__dict__['decider']
