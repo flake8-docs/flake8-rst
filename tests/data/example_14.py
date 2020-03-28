5d08c1e434d8837594b2386b8f03a2ad985be0d1
Denis Kataev <denis.a.kataev+git@gmail.com>
2020-03-28 19:59:19 +0500

    Add test

---
+++ tests/data/example_14.py
@@ -0,0 +1,17 @@
+def apply():
+    r"""
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    deserunt mollit anim id est laborum.
+
+    >>> x = 10
+    >>> e3 = x <  1
+
+    using distributed version with 4 workers
+    running online LDA training, 100 topics, 1 passes over the supplied corpus of 3199665 documets,
+    updating model once every 40000 documents
+        ..
+
+    Some another text
+    """
+
+    some_field = 1
---
+++ tests/result_py3/result_14.py
@@ -0,0 +1,3 @@
+('test_precisely',
+ [('E222', 7, 16, 'multiple spaces after operator', '    >>> e3 = x <  1\n')],
+ {'logical lines': 2, 'physical lines': 2, 'tokens': 10})
